6bb771a9b3b3016db5e20c2b3747392622089715
Sam-Safari <samuelemanman@gmail.com>
2025-10-10 13:50:59 +0300

    add updated index.js file

--- Desktop/Fetch Lab (CodeGrade)/index.js
+++ Desktop/Fetch Lab (CodeGrade)/index.js
@@ -4,14 +4,11 @@ function fetchBooks() {
     .then((books) => {
       renderBooks(books);
       return books;
-    })
-    .catch((error) => console.error("Error fetching books:", error));
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector("#book-list");
-  main.innerHTML = "";
-
+  const main = document.querySelector("main");
   books.forEach((book) => {
     const p = document.createElement("p");
     p.textContent = book.name;
