eb380004996859f9b0996ef86136ae65bf32dcd3
Sam-Safari <samuelemanman@gmail.com>
2025-10-11 20:10:46 +0300

    updated index.js file

--- Desktop/Fetch Lab (CodeGrade)/index.js
+++ Desktop/Fetch Lab (CodeGrade)/index.js
@@ -1,18 +1,14 @@
-// index.js
 function fetchBooks() {
-  return fetch("http://localhost:3000/books") // ✅ Return the fetch
+  return fetch("http://localhost:3000/books")   // return is important!
     .then(response => response.json())
-    .then(data => renderBooks(data)); // Pass JSON object to renderBooks()
+    .then(data => renderBooks(data));
 }
 
 function renderBooks(books) {
-  const list = document.getElementById("book-list");
+  const list = document.getElementById("list");
   books.forEach(book => {
     const li = document.createElement("li");
     li.textContent = book.title;
     list.appendChild(li);
   });
 }
-
-// Call fetchBooks so it runs when page loads
-fetchBooks();
